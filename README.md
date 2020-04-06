d59f8d32ceb759b05565563b30490a233963a91c
Shawn Sörbom <shawn@sorbom.com>
2020-04-06 13:50:37 -0700

    Update README and add config generation code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Mythbot
 Mythbot is a python script for use with the Mythtv project that allows for out-of-home control of a Mythtv DVR via various messaging back ends. I plan to support the IMAP/SMTP and Matrix protocols. Ideally, the bot will be modular enough to add more protocols in the future.
+
+## Installation Prerequisites
+This prerequisite guide assumes a modern version of ubuntu.
+Run the following commands in a BASH terminal 
+`sudo apt install libmariadbclient-dev`
+`pip3 install future`
+`pip3 install mysqlclient`
+Now we need to get the Mythtv source code:
+`git clone https://github.com/MythTV/mythtv`
+...Go to the directory where the API's we need are located:
+`cd ./mythtv/bindings/python/`
+...And install the python API
+`sudo python3 setup.py install`
--- mythbot-main.py
+++ mythbot-main.py
@@ -2,12 +2,19 @@ import os
 import configparser
 def genCfg(cfgPath):
    os.makedirs(os.path.dirname(cfgPath), exist_ok=True) #TODO: Surround this in try-catch block
-   print("config dir found. ")
+   print("config dir made. ")
+   mCfg = configparser.ConfigParser()
+   mCfg ["MythTvProperties"] = {"Host Name": "mythbox", "IP Address": "127.0.0.1", "Mythbackend Port": "6544", "MythFrontend Port":"6547"}
+   mCfg ["IMAP Settings"] = {"Sever IP": "127.0.0.1","Username":"username", "Password": "password", "Security": "ssl"}
+   mCfg ["SMTP Settings"] = {"Sever IP": "127.0.0.1","Username":"username", "Password": "password", "Security": "startls", "Email Address": "email@example.com"}
+   mCfg ["Matrix Settings"] = {"Sever IP": "127.0.0.1","MXID":"@username:matrix.org", "Password": "password"}
+   with open(cfgPath, "w") as configfile:
+      mCfg.write(configfile)
 def main():
     # Obligatory Setup
     #------------------
     cfgDirPath = os.path.expanduser("~/.mythbot/")
-    cfgFilePath = os.path.expanduser("~/.mythbot/")
+    cfgFilePath = os.path.expanduser("~/.mythbot/config.ini")
     cfgExists = os.path.exists(cfgFilePath)
     print (cfgFilePath)
     # TODO: Add argparser, the above settings are temporary
@@ -17,6 +24,7 @@ def main():
         print ("A defaut configuration file was generated at " + cfgFilePath)
         print ("Set your desired values and run this program again.")
         return
+    print ("Config found at: " + cfgFilePath)
     
     
 if __name__ == "__main__":
