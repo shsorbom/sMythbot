219b52361cffa2fd75b044224ab1134a13cb8026
Shawn Sörbom <shawn@sorbom.com>
2020-06-16 00:02:14 -0700

    Add support for writing room configurations

--- sMythClient.py
+++ sMythClient.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import configparser
 from importlib import util
 from nio import (AsyncClient, SyncResponse, RoomMessageText)
 
@@ -9,6 +11,12 @@ class smythClient(object):
         self.username = username
         self.password = password
         self.client = AsyncClient(self.homeserver, self.username, ssl = False)
+        
+
+        #Initialize per room configurator
+        self.roomConfigsPath = os.path.expanduser("~/.mythbot/rooms.ini")
+        self.smythbotRoomConfigs = configparser.ConfigParser()
+        self.roomCfgDefaults = self.get_default_room_options_for_smythbot()
         return
 
     async def init_login(self):
@@ -30,11 +38,11 @@ class smythClient(object):
         """
         
         await self.init_login()
-        #await self.sync_room_configs()
+        await self.sync_room_configs()
         #await self.client.sync_forever(timeout=30000)
         return
 
-    async def sync_room_configs():
+    async def sync_room_configs(self):
         """
         Name: Sync_room_configs
         Expected input: None
@@ -43,6 +51,46 @@ class smythClient(object):
         Each room that sMythbot is part of can be configured with seperate properties.
         These properties will be updated as the bot's settings are changed.
         """
-        pass
-    
+        # Fire an initial sync to get a rooms list:
+        first_sync = await self.client.sync(300000)
+        # Then we check if roooms.ini exists.  
+        if os.path.exists(self.roomConfigsPath):
+            self.smythbotRoomConfigs.read(self.roomConfigsPath) #If so, we read the existing values into the configparser
+
+        for room_id in first_sync.rooms.join: # Now we check for individual room configurations via the existence of a room ID in the file
+            # First, the easy part. If a room ID DOES NOT exist, we can safely assume there are no config options for it yet:
+            newDataWritten = False
+            if not self.smythbotRoomConfigs.has_section(room_id):
+                newDataWritten = True
+                self.smythbotRoomConfigs[room_id] = {}
+                self.smythbotRoomConfigs[room_id]["MythTv Backend address"] = "None"
+                self.smythbotRoomConfigs[room_id]["MythTv Backend Port"] = "6544"
+                self.smythbotRoomConfigs[room_id]["Room Notifications"] = "False"
+                print("Added new room Configuration " + room_id)
+                
+        if newDataWritten:
+            print("Writing New data to file: " + self.roomConfigsPath)
+            with open(self.roomConfigsPath, "w") as roomsFile:
+                self.smythbotRoomConfigs.write(roomsFile) 
+        else:
+            print ("No new room configurations found") 
+
+
+    def get_default_room_options_for_smythbot(self):
+        """
+        NOTE: Not used, I may delete this
+        Name: get_default_room_options_for_smythbot
+        Expected input: None
+        Expected output: dict of all room configuration options and their default values
+        Description: used internally for updating out of date room configurations.
+        """
+        roomOptsDict = {}
+        roomOptsDict["Backend URL"] = "None"
+        roomOptsDict["Backend Port"] = 6544
+        roomOptsDict["Notifications"] = False
+        return roomOptsDict
         
+
+
+
+           
--- smythbot-main.py
+++ smythbot-main.py
@@ -11,7 +11,7 @@ async def genCfg(cfgPath):
     mCfg["MythTvProperties"] = {"Host Name": "mythbox", "IP Address": "127.0.0.1", "Mythbackend Port": "6544", "MythFrontend Port":"6547"}
     mCfg["IMAP Settings"] = {"Sever IP": "127.0.0.1","Username":"username", "Password": "password", "Security": "ssl"}
     mCfg["SMTP Settings"] = {"Sever IP": "127.0.0.1","Username":"username", "Password": "password", "Security": "startls", "Email Address": "email@example.com"}
-    mCfg["Matrix Settings"] = {"Server Name": "127.0.0.1","User Name":"username", "Password": "password"}
+    mCfg["Matrix Settings"] = {"Server Name": "127.0.0.1","User Name":"username", "Password": "password", "SSL Check": True} # TODO: Add support to toggle SSL Checks.
     with open(cfgPath, "w") as configfile:
         mCfg.write(configfile)
 
@@ -20,7 +20,7 @@ async def main():
     # Obligatory Setup
     #------------------
 
-    cfgFilePath = os.path.expanduser("~/.mythbot/config.ini")
+    cfgFilePath = os.path.expanduser("~/.mythbot/config.ini") # TODO: Rewrite this statement for non POSIX systems
     cfgExists = os.path.exists(cfgFilePath)
     print (cfgFilePath)
     # TODO: Add argparser, the above settings are temporary
@@ -34,9 +34,9 @@ async def main():
     botCfg = configparser.ConfigParser()
     botCfg.read(cfgFilePath)
     matrix_bot = smythClient(botCfg["Matrix Settings"]["Server Name"], botCfg["Matrix Settings"]["User Name"], botCfg["Matrix Settings"]["Password"])
-    await matrix_bot.init_login()
+    await matrix_bot.start_client()
     await matrix_bot.client.close()
-    print(matrix_bot.response)
+    #print(matrix_bot.response)
     
     
     
